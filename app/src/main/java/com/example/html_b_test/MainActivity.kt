c39470b62c9e80466a9e94f1d89268e173629444
Simon Hardt <s.hardt@appdev.de>
2022-12-16 14:21:19 +0100

    add bold test

--- app/src/main/java/com/example/html_b_test/MainActivity.kt
+++ app/src/main/java/com/example/html_b_test/MainActivity.kt
@@ -1,40 +1,69 @@
 package com.example.html_b_test
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spanned
+import android.text.style.StyleSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.HtmlCompat
 import com.example.html_b_test.ui.theme.HtmlbtestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            Test()
+        }
+    }
+
+    companion object {
+        val HTML_TEXT = "Can <b>you</b> be bold?"
+        val attentionStyle = SpanStyle(fontWeight = FontWeight.Bold)
+
+        @Composable
+        fun Test() {
             HtmlbtestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val spanned = HTML_TEXT.toSpanned()
+                val annotatedString = spanned.toAnnotatedString()
+
+                Text(annotatedString)
             }
         }
     }
 }
 
+fun String.toSpanned(htmlCompatMode: Int = HtmlCompat.FROM_HTML_MODE_LEGACY) = HtmlCompat.fromHtml(this, htmlCompatMode)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@ReadOnlyComposable
+fun Spanned.toAnnotatedString(
+    attentionStyle: SpanStyle = MainActivity.attentionStyle,
+): AnnotatedString = buildAnnotatedString {
+    val spanned = this@toAnnotatedString
+    append(spanned.toString())
+    getSpans(0, spanned.length, Any::class.java).forEach { span ->
+        val start = getSpanStart(span)
+        val end = getSpanEnd(span)
+        when (span) {
+            is StyleSpan -> when (span.style) {
+                Typeface.BOLD -> addStyle(attentionStyle, start, end)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    HtmlbtestTheme {
-        Greeting("Android")
-    }
+fun TestPreview() {
+    MainActivity.Test()
 }
